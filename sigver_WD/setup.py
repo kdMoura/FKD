e75d9694cbc84427d0ed3edc732d1e88aa0d1168
Kecia <kecia.de.moura+github@gmail.com>
2025-08-04 14:57:06 -0400

    rename to sigfkd

--- sigver_WD/setup.py
+++ sigver_WD/setup.py
@@ -7,14 +7,14 @@ setup_path = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(setup_path, 'README.md'), encoding='utf-8-sig') as f:
     README = f.read()
 
-setup(name='sigver',
+setup(name='sigfkd',
       version='1.0',
       url='https://github.com/dimTsourounis/FKD_sigver',
       maintainer='Dimitrios Tsourounis',
       maintainer_email='dtsourounis@upatras.gr',
       description='Signature verification package for Feature-based Knowledge Distillation'
                   'training writer-dependent classifiers.',
-      long_description=README,
+      #long_description=README,
       author='Dimitrios Tsourounisn',
       author_email='dtsourounis@upatras.gr',
       license='BSD 3-clause "New" or "Revised License"',
